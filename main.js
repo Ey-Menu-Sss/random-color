2b8ee2df34908d56fd8d1e894fa0b8ba67b15cdb
Ey-Menu-Sss <115058557+Ey-Menu-Sss@users.noreply.github.com>
2022-11-29 23:16:23 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 
 
 
-let jj = document.querySelector(".random");
+  let jj = document.querySelector(".random");
 
-setInterval(() => {
+
+
+function bla() {
     let ran_1 = Math.floor(Math.random() * 250) + 1;
-    let ran_2= Math.floor(Math.random() * 250) + 1;
+    let ran_2 = Math.floor(Math.random() * 250) + 1;
     let ran_3 = Math.floor(Math.random() * 250) + 1;
+    jj.style.backgroundColor = `rgb(${ran_1}, ${ran_2}, ${ran_3})`;
+}
 
-    jj.style.backgroundColor = `rgb(${ran_1}, ${ran_2}, ${ran_3})`
-}, 1000);
 
 
